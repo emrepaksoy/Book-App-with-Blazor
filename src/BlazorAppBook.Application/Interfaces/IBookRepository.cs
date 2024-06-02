983938fbadb2d8880b2089fa93572140fc96beba
Emre Paksoy <emrepaksoy4@gmail.com>
2024-06-02 14:19:05 +0300

    insert update delete işlemleri tamamlandı

--- src/BlazorAppBook.Application/Interfaces/IBookRepository.cs
+++ src/BlazorAppBook.Application/Interfaces/IBookRepository.cs
@@ -5,5 +5,9 @@ namespace BlazorAppBook.Application.Interfaces
     public interface IBookRepository
     {
         Task AddSync(BookEntity book);
+        Task<List<BookEntity>> GetAllAsync();
+        Task<BookEntity?> GetByIdAsync(int id);
+        Task UpdateAsync(BookEntity book);
+        Task DeleteAsync(int id);
     }
 }
--- src/BlazorAppBook.Infrastructure/Repositories/BookRepository.cs
+++ src/BlazorAppBook.Infrastructure/Repositories/BookRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorAppBook.Application.Interfaces;
 using BlazorAppBook.Domain.Entitites.Book;
 using BlazorAppBook.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorAppBook.Infrastructure.Repositories
 {
@@ -17,6 +18,33 @@ namespace BlazorAppBook.Infrastructure.Repositories
         {
             _context.Book.Add(book);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var book = await GetByIdAsync(id);
+            if(book is not null)
+            {
+                _context.Book.Remove(book);
+               await  _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<BookEntity>> GetAllAsync()
+        {
+            return await _context.Book.ToListAsync();
+        }
+
+        public async Task<BookEntity?> GetByIdAsync(int id)
+        {
+           var book = await _context.Book.FirstOrDefaultAsync(e => e.Id == id);
+            return book;
+        }
+
+        public async Task UpdateAsync(BookEntity book)
+        {
+           _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();  
         }
     }
 }
